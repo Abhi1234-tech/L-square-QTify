857374fd000611b4d02e295a82d1d4f89461cdff
Abhishek Girnar <64825778+Abhi1234-tech@users.noreply.github.com>
2023-08-15 07:40:51 +0000

    Added Songs section with genre

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -7,8 +7,12 @@ import './App.css';
 function App() {
   const [topdata, setTopdata] = useState([]);
   const [newAlbums, setNewAlbums] = useState([]);
+  const [songs, setSongs] = useState([]);
   const [showAllTopCards, setShowAllTopCards] = useState(false);
   const [showAllNewCards, setShowAllNewCards] = useState(false);
+  const [showAllSongs, setShowAllSongs] = useState(false);
+  const [genres, setGenres] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(""); // Store the selected genre
 
   useEffect(() => {
     axios.get("https://qtify-backend-labs.crio.do/albums/top")
@@ -26,6 +30,22 @@ function App() {
       .catch(error => {
         console.error("Error fetching new albums:", error);
       });
+
+    axios.get("https://qtify-backend-labs.crio.do/songs")
+      .then(response => {
+        setSongs(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching songs:", error);
+      });
+
+    axios.get("https://qtify-backend-labs.crio.do/genres") // Fetch genres data
+      .then(response => {
+        setGenres(response.data.data);
+      })
+      .catch(error => {
+        console.error("Error fetching genres:", error);
+      });
   }, []);
 
   const toggleShowAllTopCards = () => {
@@ -35,6 +55,14 @@ function App() {
   const toggleShowAllNewCards = () => {
     setShowAllNewCards(!showAllNewCards);
   };
+
+  const toggleShowAllSongs = () => {
+    setShowAllSongs(!showAllSongs);
+  };
+
+  const filteredSongs = selectedGenre
+    ? songs.filter(song => song.genre.key === selectedGenre)
+    : songs;
 
   return (
     <div className="App">
@@ -79,6 +107,39 @@ function App() {
           </p>
         </div>
         <Card topdata={showAllNewCards ? newAlbums : newAlbums.slice(0, 6)} />
+      </div>
+      <div className="card-songs">
+        <div className="card-headers">
+          <h4>Songs</h4>
+          <div className="genre-buttons">
+            <button
+              className={`genre-button ${selectedGenre === "" ? "selected" : ""}`}
+              onClick={() => setSelectedGenre("")}
+            >
+              All
+            </button>
+            {genres.map(genre => (
+              <button
+                key={genre.key}
+                className={`genre-button ${selectedGenre === genre.key ? "selected" : ""}`}
+                onClick={() => setSelectedGenre(genre.key)}
+              >
+                {genre.label}
+              </button>
+            ))}
+          </div>
+          <p
+            data-bs-toggle="collapse"
+            href="#collapseExample"
+            role="button"
+            aria-expanded="false"
+            aria-controls="collapseExample"
+            onClick={toggleShowAllSongs}
+          >
+            {showAllSongs ? "Collapse" : "Show More"}
+          </p>
+        </div>
+        <Card topdata={showAllSongs ? filteredSongs : filteredSongs.slice(0, 6)} />
       </div>
     </div>
   );
--- qtify/src/header.js
+++ qtify/src/header.js
@@ -1,45 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 function Header() {
+  const [searchQuery, setSearchQuery] = useState(""); // State to store the search query
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    
+    console.log("Search Query:", searchQuery);
+  };
+
   return (
     <div className="header">
-    <div className="logo">
-        
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0GpgTZdIlebkTxiAfBcD_Isp6DrWaSreT9w&usqp=CAU"/>
+      <div className="logo">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0GpgTZdIlebkTxiAfBcD_Isp6DrWaSreT9w&usqp=CAU" alt="Logo" />
         <p>Q Tify</p>
-    </div>    
-    <div className="searchbar">
-    <nav className="navbar navbar-custom">
-      <div className="container-fluid">
-        <form className="d-flex" role="search">
-          <input
-            className="form-control me-2"
-            type="search"
-            placeholder="Search a album of your choice"
-            aria-label="Search"
-          />
-          <button className="btn btn-outline-success" type="submit">
-          <svg
-      fill="black"
-      viewBox="0 0 16 16"
-      height="1em"
-      width="1em"
-      
-
-    >
-      <path d="M11.742 10.344a6.5 6.5 0 10-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 001.415-1.414l-3.85-3.85a1.007 1.007 0 00-.115-.1zM12 6.5a5.5 5.5 0 11-11 0 5.5 5.5 0 0111 0z" />
-    </svg>
-          </button>
-        </form>
       </div>
-    </nav>
-    </div>
-    <div className="feedback">
+      <div className="searchbar">
+        <nav className="navbar navbar-custom">
+          <div className="container-fluid">
+            <form className="d-flex" role="search" onSubmit={handleSearchSubmit}>
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search an album of your choice"
+                aria-label="Search"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+              />
+              <button className="btn btn-outline-success" type="submit">
+                <svg
+                  fill="black"
+                  viewBox="0 0 16 16"
+                  height="1em"
+                  width="1em"
+                >
+                  <path d="M11.742 10.344a6.5 6.5 0 10-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 001.415-1.414l-3.85-3.85a1.007 1.007 0 00-.115-.1zM12 6.5a5.5 5.5 0 11-11 0 5.5 5.5 0 0111 0z" />
+                </svg>
+              </button>
+            </form>
+          </div>
+        </nav>
+      </div>
+      <div className="feedback">
         <button>Give Feedback</button>
-    </div>
+      </div>
     </div>
   );
 }
 
-export {Header};
+export { Header };
